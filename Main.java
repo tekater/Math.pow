4ccb7933f7c80a42a3372f933d49b4537c553b60
tekater <120014627+tekater@users.noreply.github.com>
2023-01-23 19:36:11 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,6 +11,18 @@ public class Main {
         System.out.println("Введите степень b:");
         double b = scanner.nextDouble();
         double c = Math.pow(2,3);
-        System.out.println("Возвысили степень b для числа a" + " " + c);
+        System.out.println("Возвысили степень b для числа a:");
+        System.out.println("c=" + c);
+        System.out.println(" ");
+
+        // Второй вариант
+        System.out.println("Введите число a, затем степень b");
+        System.out.println("c=" + Math.pow(scanner.nextDouble() ,scanner.nextDouble()));
+        System.out.println(" ");
+
+        // Третий Вариант
+        System.out.println("Введите число a, затем степень b");
+        System.out.println("c=" + Math.pow(new Scanner(System.in).nextDouble(), new Scanner(System.in).nextDouble()));
+        System.out.println(" ");
     }
 }
