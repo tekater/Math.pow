d0eca40c0ad19799c7558860c3a50c002c18f2f4
tekater <120014627+tekater@users.noreply.github.com>
2023-01-23 19:28:13 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         //double a = 2;
-        System.out.println("Введите число a:"); //2
+        System.out.println("Введите число a:");
         double a = scanner.nextDouble();
 
         //double b = 3;
-        System.out.println("Введите число b:"); //3
+        System.out.println("Введите степень b:");
         double b = scanner.nextDouble();
         double c = Math.pow(2,3);
-        System.out.println("Возвысили степень b число a" + " " + c);
+        System.out.println("Возвысили степень b для числа a" + " " + c);
     }
 }
